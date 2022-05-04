a241370735f2e76d300929a422e06d3766cc7a1e
hyeon <100738360+Choiyuhyeon@users.noreply.github.com>
2022-05-04 09:27:12 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name = 'mycalculator4878'
+  version = '0.0.1',
+  description = 'oss-dev my calculator',
+  author = 'Choi yu hyeon'
+  author_email = 'wf4878@kakao.com'
+  url = https://github.com/Choiyuhyeon/mycalculator-practice
+  install_rquores = ['pytest'],
+  long_description = 'oss-dev my cal',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
